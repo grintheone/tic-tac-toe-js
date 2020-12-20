496a056e4b1eaa41b8d4b2e91be3e18439072495
Alex <grin3run@gmail.com>
2020-12-20 14:02:43 +0300

    Added ability to change player's names and markers.

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-// An object representing text 
+// An object representing text
 const gameTextObj = (() => {
+    // Event listenters for players 
+    const left = Array.from(document.querySelector('.left').children);
+    for (let i = 0; i < left.length; i++) {
+        left[i].addEventListener('click', (e) => {
+            if (left[0].hasAttribute('id', 'active-left') || left[1].hasAttribute('id', 'active-left')) {
+                left[0].removeAttribute('id', 'active-left')
+                left[1].removeAttribute('id', 'active-left')
+            }
+            e.target.setAttribute('id', 'active-left')
+        })
+    }
+    
+
+    const right = Array.from(document.querySelector('.right').children);
+    for (let i = 0; i < right.length; i++) {
+        right[i].addEventListener('click', (e) => {
+            if (right[0].hasAttribute('id', 'active-right') || right[1].hasAttribute('id', 'active-right')) {
+                right[0].removeAttribute('id', 'active-right')
+                right[1].removeAttribute('id', 'active-right')
+            }
+            e.target.setAttribute('id', 'active-right');
+        })
+    }
+
+    
+    // Chech if the player is chosen
+    function playersCheck() {
+        if (document.getElementById('active-right') != null & document.getElementById('active-left') != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
     
     const gameStage = () => {
         document.querySelector('.main-container').style = "display: none";
 
         const currentPlayer = document.createElement('div');
         currentPlayer.setAttribute('id', 'currentPlayer');
-        currentPlayer.textContent = firstPlayer.name + ` (${firstPlayer.marker}):`
+        currentPlayer.textContent = firstPlayer.name + ` (${firstPlayer.marker}) turn:`
         document.body.appendChild(currentPlayer);
     }
 
@@ -15,14 +49,14 @@ const gameTextObj = (() => {
         announce.textContent = announceWinner();
 
         // Play again with the same players
-        const startOver = document.createElement('button');
+        const startOver = document.createElement('div');
         startOver.setAttribute('id', 'new-game');
         startOver.textContent = 'Play again';
         startOver.addEventListener('click', game.startOver);
         document.body.appendChild(startOver);
 
         // Change players (main menu analog)
-        const changePlayers = document.createElement('button');
+        const changePlayers = document.createElement('div');
         changePlayers.setAttribute('id', 'change-players');
         changePlayers.textContent = 'Change Players';
         changePlayers.addEventListener('click', game.backToMenu);
@@ -31,9 +65,9 @@ const gameTextObj = (() => {
 
     function changeName() {
         if (counter % 2 == 1) {
-            document.getElementById('currentPlayer').textContent = firstPlayer.name + ` (${firstPlayer.marker}):`;
+            document.getElementById('currentPlayer').textContent = firstPlayer.name + ` (${firstPlayer.marker}) turn:`;
         } else {
-            document.getElementById('currentPlayer').textContent = secondPlayer.name + ` (${secondPlayer.marker}):`;
+            document.getElementById('currentPlayer').textContent = secondPlayer.name + ` (${secondPlayer.marker}) turn:`;
         }
     }
 
@@ -42,12 +76,12 @@ const gameTextObj = (() => {
             return 'It\'s a tie!';
         }
         if (counter % 2 == 1) {
-            return `${secondPlayer.name} (${secondPlayer.marker}) has won!`
+            return `${secondPlayer.name} (${secondPlayer.marker}) wins!`
         } else {
-            return `${firstPlayer.name} (${firstPlayer.marker}) has won!`
+            return `${firstPlayer.name} (${firstPlayer.marker}) wins!`
         } 
     }
-    return {gameStage, winStage, changeName};
+    return {gameStage, winStage, changeName, playersCheck};
 })();
 
 
@@ -151,11 +185,17 @@ const gameBoardObj = (() => {
 // Main game module controlling the flow of the game
 const game = (() => {
     const startBtn = document.getElementById('start-game');
-
+    
     const startGame = () => {
-        gameTextObj.gameStage();
-        gameBoardObj.createBoard();
-        gameBoardObj.PlayerMove();
+        if (gameTextObj.playersCheck()) {
+            gameTextObj.gameStage();
+            gameBoardObj.createBoard();
+            gameBoardObj.PlayerMove();
+        } else {
+            alert('Please select player types')
+        }
+        
+        
     }
 
     const startOver = () => {
@@ -181,15 +221,75 @@ const game = (() => {
 })();
 
 // Factory function for creating players
-const Player = (name, marker) => {
-    return {name, marker}
+const Player = (name, marker, markers) => {
+    return {name, marker, markers}
 }
 
 let counter = 0
-const firstPlayer = Player('Alex', 'X');
-const secondPlayer = Player('Vadim', 'O');
+const firstPlayer = Player('Player 1','X', ['X', '🎄', '🐶', '💧']);
+const secondPlayer = Player('Player 2','O', ['O', '🎁', '🐱', '🔥']);
 
+const formObj = (() => {
+    let currentPlayer = '';
+    const accept = document.getElementById('accept');
+    const cancel = document.getElementById('cancel');
 
+    accept.addEventListener('click', makeChanges);
+    cancel.addEventListener('click', closeForm);
+    
+    // Make changes to the player variable
+    function makeChanges() {
 
+        const newName = document.getElementById('player-name').value;
+        const radio = document.querySelectorAll('input[type="radio"]');
+        // Change players marker
+        for (let i = 0; i < radio.length; i++) {
+            if (radio[i].checked == true) {
+                if (currentPlayer == firstPlayer) {
+                    firstPlayer.marker = radio[i].nextElementSibling.firstElementChild.textContent;
+                    document.getElementById('first-player-marker').textContent = ` (${firstPlayer.marker})`
+                } else {
+                    secondPlayer.marker = radio[i].nextElementSibling.firstElementChild.textContent;
+                    document.getElementById('second-player-marker').textContent = ` (${secondPlayer.marker})`
+                }
+            }
+        }
 
+        if (newName != '') {
+            currentPlayer.name = newName;
+            if (currentPlayer == firstPlayer) {
+                document.getElementById('first-player-name').textContent = newName;
+            } else {
+                document.getElementById('second-player-name').textContent = newName;
+            }
+        }
+        
+        closeForm();
+    }
+
+    // Open close forms
+    function openForm() {
+        document.getElementById('form').reset();
+        currentPlayer = this[0];
+        // Chenge the placeholder to the current player name
+        const placeholder = document.getElementById('player-name');
+        placeholder.setAttribute('placeholder', currentPlayer.name)
+        const emoji = document.querySelectorAll('.emoji');
+        for (let i = 0; i < emoji.length; i++) {
+            emoji[i].textContent = this[0].markers[i];
+            if (this[0].marker == emoji[i].textContent) {
+                emoji[i].parentElement.previousElementSibling.checked = true;
+            }
+        }
+        document.getElementById('form').style.display = 'block';
+    }
+
+    function closeForm() {
+        document.getElementById('form').style.display = 'none';
+    }
+
+    const playerForms = document.querySelectorAll('.form');
+    playerForms[0].addEventListener('click', openForm.bind([firstPlayer]))
+    playerForms[1].addEventListener('click', openForm.bind([secondPlayer]))
+})();
 
